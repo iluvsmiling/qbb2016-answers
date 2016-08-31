9d686475b69f24e6a80140100f4fe52292316720
Nina Rao <charmquark29@gmail.com>
2016-08-31 08:05:19 -0400

    This hmw is on fire

---
+++ day2-hmw/numb1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+file_name = sys.argv[1]
+file = open(file_name)
+for line in file.readlines():
+        if "DROME" in line:
+                continue
+        item = line.rstrip("\r\n").split()
+	if len(item) >= 4 :
+
+		print(item[3]+"\t"+item [2])
+
