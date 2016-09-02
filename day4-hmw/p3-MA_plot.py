2b27b55f2c97d122d0f91a3b7dbf4a99c07f0b8f
Nina Rao <charmquark29@gmail.com>
2016-09-02 09:30:59 -0400

    fixed comments

--- day4-hmw/p3-MA_plot.py
+++ day4-hmw/p3-MA_plot.py
@@ -4,17 +4,13 @@ Usage: <SRR072893> <SRR072915>
 
 #!/usr/bin/env python
 
-
 import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#open SRR files of interest
 df_n1 = pd.read_table(sys.argv[1])
 df_n2 = pd.read_table(sys.argv[2])
-
-#create an array of fpkm values
 
 fpkm_n1 = df_n1["FPKM"].values
 fpkm_n2 = df_n2["FPKM"].values
@@ -33,7 +29,6 @@ plt.scatter(A_n1, M_n1)
 plt.ylabel("M")
 plt.xlabel("A")
 plt.title("MA Plot of SRR072893 and SRR072915 Expression")
-
 
 plt.savefig("p3_MA_res.png")
 plt.close()
