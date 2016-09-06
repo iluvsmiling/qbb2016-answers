94af879cf5adc200efd4c55d253c72934ca242e5
Nina Rao <charmquark29@gmail.com>
2016-09-05 21:32:01 -0400

    .

--- day3-hmw/01-kmer-matcher.py
+++ day3-hmw/01-kmer-matcher.py
@@ -20,31 +20,33 @@ for ident, sequence in fasta_fixed.FASTAReader(query):
     sequence = sequence.upper()
     for i in range(0, len(sequence) - k_len):
         kmer = sequence[i: i+k_len]
-        if kmer not in query_start:
-        #if not query_start.get(kmer,0):
+        #if kmer not in query_start:
+        #print kmer
+        if not query_start.get(kmer,0):
             query_start [kmer] = i
                 
-
+print len (query_start)
 # print sequence[0:4]
 #print (query_start[stringg])
 
-print kmer, query_start[kmer]
+#print query_start
+#print kmer, query_start[kmer]
 
 #parse line in target
 #find kmer within sequence
 #print ident (target_name) with kmer
 
-# for ident, sequence in fasta_fixed.FASTAReader(target):
-#         sequence = sequence.upper()
-#         target_start = i
-#         end = i+k_len
-#         kmer = sequence[i : i+k_len]
-#         if kmer in query_start:
-#             print ("Target Seq Name: " + ident + "Target Start: " + target_start + "Query Start:" + query_start +"Kmer:" + kmer)
-#
-#
-#         else:
-#             print "error"
+for i, sequence in fasta_fixed.FASTAReader(target):
+        sequence = sequence.upper()
+        target_start = i
+        kmer = sequence[i : i+k_len]
+        for i in range(0, len(sequence)-k_len):
+            if kmer in query_start:
+                print ("Target Seq Name: " + ident + "Target Start: " + target_start + "Query Start:" + query_start +"Kmer:" + kmer)
+
+
+            else:
+                print "error"
 
 
 
