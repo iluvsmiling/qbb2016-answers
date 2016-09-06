d044eac0d4ab62ad9c7dc744253b821345b4ac9e
Nina Rao <charmquark29@gmail.com>
2016-09-05 21:10:29 -0400

    dictionary only contains last kmer

--- day3-hmw/01-kmer-matcher.py
+++ day3-hmw/01-kmer-matcher.py
@@ -11,6 +11,7 @@ import fasta_fixed
 target = open(sys.argv[1]) 
 query = open(sys.argv[2])
 k_len = int(sys.argv[3])
+#stringg = str(sys.argv[4])
 
 ### Make dictionary of kmer_seq and query start 
 
@@ -18,31 +19,32 @@ query_start = {}
 for ident, sequence in fasta_fixed.FASTAReader(query):
     sequence = sequence.upper()
     for i in range(0, len(sequence) - k_len):
-        q_start = i
-        end = i + k_len
         kmer = sequence[i: i+k_len]
         if kmer not in query_start:
-            query_start [kmer] = [q_start]
-        else:
-            query_start[kmer].append(q_start)
+        #if not query_start.get(kmer,0):
+            query_start [kmer] = i
+                
 
-print kmer, query_start[kmer],
+# print sequence[0:4]
+#print (query_start[stringg])
+
+print kmer, query_start[kmer]
 
 #parse line in target
 #find kmer within sequence
 #print ident (target_name) with kmer
 
-for ident, sequence in fasta_fixed.FASTAReader(target):
-        sequence = sequence.upper()
-        target_start = i
-        end = i+k_len
-        kmer = sequence[i : i+k_len]
-        if kmer in query_start: 
-            print ("Target Seq Name: " + ident + "Target Start: " + target_start + "Query Start:" + query_start +"Kmer:" + kmer)
-    
-   
-        else:
-            print "error"
+# for ident, sequence in fasta_fixed.FASTAReader(target):
+#         sequence = sequence.upper()
+#         target_start = i
+#         end = i+k_len
+#         kmer = sequence[i : i+k_len]
+#         if kmer in query_start:
+#             print ("Target Seq Name: " + ident + "Target Start: " + target_start + "Query Start:" + query_start +"Kmer:" + kmer)
+#
+#
+#         else:
+#             print "error"
 
 
 
